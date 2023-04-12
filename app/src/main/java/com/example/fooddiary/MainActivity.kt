4f9af24d49be41ec7215dac73ef507d5c8596949
Carson <koolcb@gmail.com>
2023-04-12 15:27:59 -0600

    Please look at my database

--- app/src/main/java/com/example/fooddiary/MainActivity.kt
+++ app/src/main/java/com/example/fooddiary/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.fooddiary
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 lateinit var buttonFoodDiary: Button
 lateinit var buttonNewEntry: Button
+lateinit var buttonOnlineRes: Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +16,10 @@ class MainActivity : AppCompatActivity() {
 
         buttonFoodDiary=findViewById(R.id.foodDiary)
         buttonNewEntry=findViewById(R.id.newEntry)
+        buttonOnlineRes=findViewById(R.id.onlineRes)
+        val webIntent: Intent = Uri.parse("https://www.myplate.gov/eat-healthy/food-group-gallery").let{webpage->
+            Intent(Intent.ACTION_VIEW, webpage)
+        }
         buttonFoodDiary.setOnClickListener{
             intent = Intent(this,food_Diary::class.java)
             startActivity(intent)
@@ -24,5 +30,10 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
 
         }
+        buttonOnlineRes.setOnClickListener{
+            startActivity(webIntent)
+
+            }
+
+        }
     }
-}
